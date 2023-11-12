42525c2bb12486e69b6a5cc6717591c55cce86a2
dbatista-docente <dener70@gmail.com>
2023-11-12 01:26:31 -0300

    feat: add component buttonCart in header

---
+++ src/components/ButtonCart/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const SButtonCart = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90px;
+  height: 45px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: #fff;
+  margin-right: 88px;
+`;
+
+export const SNumberCart = styled.h2`
+width: 50%;
+  color: #000;
+  font-size: 18px;
+  font-weight: 700;
+  line-height: normal;
+`;

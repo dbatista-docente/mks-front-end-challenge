f58f5bc827785a04b9df70a40ce9ba79a1883c42
dbatista-docente <dener70@gmail.com>
2023-11-12 01:27:04 -0300

    feat: add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { SContent, SFooter } from "./style"
+
+const Footer = () => {
+    return <SFooter>
+        <SContent>MKS sistemas © Todos os direitos reservados</SContent>
+    </SFooter>
+}
+
+export default Footer
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const SFooter = styled.footer`
+display: flex;
+  width: 100%;
+  height: 34px;
+  background: #eee;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SContent = styled.h2`
+  color: #000;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;

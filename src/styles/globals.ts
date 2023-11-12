33deb0177cf2c8cff2684f7c0b297ff022a680d7
dbatista-docente <dener70@gmail.com>
2023-11-12 01:26:46 -0300

    feat: add globla styles

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -3,6 +3,10 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   * {
+@font-face {
+  font-family: "Montserrat";
+  src: url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap") ;
+}
     margin: 0;
     padding: 0;
     font-family: 'Montserrat';

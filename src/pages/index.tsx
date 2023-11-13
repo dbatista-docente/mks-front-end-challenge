41ebbb7536e542b3c97855982e19bd6a5f80b3f3
dbatista-docente <dener70@gmail.com>
2023-11-12 21:23:32 -0300

    refactor: remove imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,4 @@
-// import Footer from "@/components/organisms/Footer";
-// import Header from "@/components/organisms/Header";
-// import ProductCard from "@/components/ProductCard";
 import ProductLists from "@/components/templates/ProductsList";
-import { SMain } from "@/styles/stylePages";
-import Head from "next/head";
-
 
 export default function Home() {
   return (

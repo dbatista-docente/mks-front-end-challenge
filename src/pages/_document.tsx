5d7da100d77b9b074176897e5794c0c0e5abd90c
dbatista-docente <dener70@gmail.com>
2023-11-13 01:06:23 -0300

    refactor: SEO remove meta width and hight og:image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,13 +6,11 @@ export default function Document() {
       <Head>
         <title>MKS Sistemas</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Teste técnico MKS Sistemas"/>
-        <meta property="og:description" content="Teste técnico Dener Batista para Front-End Developer" />
-        <meta property="og:url" content="https://mks-front-end-challenge-six.vercel.app/"/>
-        <meta property="og:image" content="https://cdn.discordapp.com/attachments/947324026236330018/1173467670629339146/Diseno_sin_titulo-removebg-preview.png?ex=65640fee&is=65519aee&hm=39f5534a8632d97f360bf883e1e5968db1dde53980374b28512b6065333df992&"/>
-        <meta property="og:image:type" content="image/png"/>
-        <meta property="og:image:width" content="671"/>
-        <meta property="og:image:height" content="865"/>
+        <meta property="og:title" content="Teste técnico MKS Sistemas" />
+        <meta property="og:description" content="Teste técnico de Dener Batista para Front-End Developer" />
+        <meta property="og:url" content="https://mks-front-end-challenge-six.vercel.app/" />
+        <meta property="og:image" content="https://cdn.discordapp.com/attachments/947324026236330018/1173467670629339146/Diseno_sin_titulo-removebg-preview.png?ex=65640fee&is=65519aee&hm=39f5534a8632d97f360bf883e1e5968db1dde53980374b28512b6065333df992&" />
+        <meta property="og:image:type" content="image/png" />
         <link rel="icon" href="./favicon.svg" />
       </Head>
       <body>
